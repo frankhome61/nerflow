fe5f828e60024ba93174c924f27b94ea5994ad97
Guowei <frankhome61@hotmail.com>
2021-11-15 12:57:24 -0800

    added render scripts

---
+++ runner_scripts/runner-iccv-01-render.sh
@@ -0,0 +1,2 @@
+pip install --upgrade imageio-ffmpeg
+python3 run_nerf.py --config=configs/iccv_01_baseline_server.txt --render_only
---
+++ runner_scripts/runner-synthetic-01-render.sh
@@ -0,0 +1,2 @@
+pip install --upgrade imageio-ffmpeg
+python3 run_nerf.py --config=configs/synthetic_01_baseline_server.txt --render_only
